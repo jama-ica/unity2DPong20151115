a6d77d33ee3fe4572ebea4042b2f5a4af9f3b95f
h.igarashi <jama-ican@hotmail.co.jp>
2015-11-15 20:31:06 +0900

    バーをマウスでも動かせるように

--- Assets/bar.cs
+++ Assets/bar.cs
@@ -3,16 +3,26 @@ using System.Collections;
 
 public class bar : MonoBehaviour {
 
+	float prevMouseX;
+
 	// Use this for initialization
 	void Start () {
-	
+		prevMouseX = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float x = Input.GetAxisRaw ("Horizontal");
 		Vector2 vec2 = transform.position;
+
+		float x = Input.GetAxisRaw ("Horizontal");
 		vec2.x += x/10.0f;
+
+		float mouseX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+		if (prevMouseX != mouseX) {
+			vec2.x = mouseX;
+			prevMouseX = mouseX;
+		}
+
 		transform.position = vec2;
 	}
 }
