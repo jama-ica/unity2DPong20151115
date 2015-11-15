3daa3da6a8fa96a93cf7cc52efaf4d304240a2ba
h.igarashi <jama-ican@hotmail.co.jp>
2015-11-15 20:16:28 +0900

    バーを追加しキーボードで操作できるように

---
+++ Assets/bar.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxisRaw ("Horizontal");
+		Vector2 vec2 = transform.position;
+		vec2.x += x/10.0f;
+		transform.position = vec2;
+	}
+}
