ff875c70ca9481e0c65e11d2ccfd05b4857d0517
h.igarashi <jama-ican@hotmail.co.jp>
2015-11-15 19:26:38 +0900

    画面にボールを表示して動かす

---
+++ Assets/ball.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Vector2 dir = new Vector2(1.0f,1.0f);		
+		GetComponent<Rigidbody2D>().velocity = dir;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
