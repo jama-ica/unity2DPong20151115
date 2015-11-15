2862ad3b54729b85fc162599b90a594bf27313a4
h.igarashi <jama-ican@hotmail.co.jp>
2015-11-15 19:58:25 +0900

    ボールを壁で反射させる

--- Assets/ball.cs
+++ Assets/ball.cs
@@ -11,6 +11,16 @@ public class ball : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		// 画面左下のワールド座標をビューポートから取得
+		Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+		// 画面右上のワールド座標をビューポートから取得
+		Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+		if (min.x > this.transform.position.x || this.transform.position.x > max.x) {
+			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x * -1.0f,GetComponent<Rigidbody2D>().velocity.y);
+		}
+		if (min.y > this.transform.position.y || this.transform.position.y > max.y) {
+			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x,GetComponent<Rigidbody2D>().velocity.y*-1.0f);
+		}
 	}
 }
