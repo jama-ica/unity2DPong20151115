3fd4508a7273953faf4896da47644e1025ce1c1b
h.igarashi <jama-ican@hotmail.co.jp>
2015-11-26 22:44:42 +0900

    バグ修正
    ブロックが１つ足りないのを修正

--- Assets/BlockController.cs
+++ Assets/BlockController.cs
@@ -3,29 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class BlockController : MonoBehaviour {
-	
+
+	private GameObject src;
 	private List<blockScript> blockList;
 
 	// Use this for initialization
 	void Start () {
 
+		this.src = this.transform.Find("block").gameObject;
+
 		Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
 		Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
 
 		this.blockList = new List<blockScript> ();
-		for (int i = 0; i < 2; i++) {
+		for (int i = 0; i < 2 ; i++) {
 			for(int j = 0 ; j < 16 ; j++){
 				//var gameObj = GameObject.Find("block");
 				//get no active game object
-				var gameObj = this.transform.Find("block").gameObject;
-				if( gameObj != null){
-					blockScript b = gameObj.GetComponent<blockScript>();
-					if(b != null){
-						b.place( min.x + j*1.0f + 1.0f, max.y - i*1.0f - 1.0f);
-						gameObj.SetActive(true);
-						Instantiate(gameObj);
-						this.blockList.Add(b);
-					}
+				GameObject gameObj = Instantiate(this.src);
+				blockScript b = gameObj.GetComponent<blockScript>();
+				if(b != null){
+					gameObj.SetActive(true);
+					b.place( min.x + j*1.0f + 1.0f, max.y - i*1.0f - 1.0f);
+					this.blockList.Add(b);
 				}
 			}
 		}
